ba4950b1e371fc0e8e541a35fe706351fddb0805
Jesus Alvarez <chuyqa@gmail.com>
2016-11-10 19:23:45 -0800

    Send aria2 results to screen

--- scripts/plowaria.sh
+++ scripts/plowaria.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
-
 # This script will be called to parallelize final download using aria2
-# 4th arg passed in is the final download link
+# $1 module name (lowercase). For example: mediafire.
+# $2 download URL.
+# $3 cookie file fulfilled by download module function.
+# $4 final URL.
+# $5 final filename.
+
 
 MAX_CONNECTIONS=4
 
-aria2c --continue --max-connection-per-server=${MAX_CONNECTIONS} $4
+aria2c --continue --max-connection-per-server=${MAX_CONNECTIONS} $4 >/dev/tty
